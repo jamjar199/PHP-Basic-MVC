b1e7960998c5445eaeb143ae161e2b3e38872964
james Lilliott <jameslilliott@hotmail.co.uk>
2018-07-21 14:15:07 +0100

    Manual add of empty config file
    Manually added config file to avoid uploading passwords to database

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'database' => [
+		'dbname' => '',
+		'user' => '',
+		'password' => '',
+		'host' => '',
+		'connection' => '',
+		'options' => [
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+		]
+	]
+];
