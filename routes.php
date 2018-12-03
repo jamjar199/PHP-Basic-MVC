ebf7a2f9aa8309ab486a4c7abc75a8a98ebec895
james Lilliott <jameslilliott@hotmail.co.uk>
2018-12-03 17:04:34 +0000

    Removed old routes

--- routes.php
+++ routes.php
@@ -1,6 +1,3 @@
 <?php
 
 $router->get('', 'controllers/index.php');
-$router->get('about', 'controllers/about.php');
-$router->get('contact', 'controllers/contact.php');
-$router->post('names', 'controllers/add-name.php');
